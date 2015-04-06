ed3075913ee4645593a07a36179b78ef484cdf02
J Taylor OConnor <jtomusic@gmail.com>
2015-04-05 21:51:55 -0500

    make navbar collapse on click

--- js/custom.js
+++ js/custom.js
@@ -35,4 +35,11 @@
 		});
 	});
 
+	//make navbar collapse on click
+	$(document).on('click','.navbar-collapse.in',function(e) {
+		if( $(e.target).is('a') ) {
+			$(this).collapse('hide');
+		}
+	});
+
 })(jQuery);
